c48ff16a357b277260c3a5eef6585f3f77547426
xiaocheng99 <56629576+xiaocheng99@users.noreply.github.com>
2022-02-14 08:42:10 +0800

    Update single_idcard.py

--- IDRecBypaddleOcr/single_idcard.py
+++ IDRecBypaddleOcr/single_idcard.py
@@ -7,8 +7,8 @@ import paddlehub as hub
 import argparse
 import sys
 #设置、gpu
-import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+#import os
+#os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 def removeSpace (long_str):
     #去除空格
@@ -93,7 +93,7 @@ def identity_OCR(reversePath,frontPath):
     #检测
     results = ocr.recognize_text(
                         images=np_images,         # 图片数据，ndarray.shape 为 [H, W, C]，BGR格式；
-                        use_gpu=True,            # 是否使用 GPU；若使用GPU，请先设置CUDA_VISIBLE_DEVICES环境变量
+                        use_gpu=False,            # 是否使用 GPU；若使用GPU，请先设置CUDA_VISIBLE_DEVICES环境变量
                         output_dir='ocr_result',  # 图片的保存路径，默认设为 ocr_result；
                         visualization=False,       # 是否将识别结果保存为图片文件；
                         box_thresh=0.5,           # 检测文本框置信度的阈值；
